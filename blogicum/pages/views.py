4a15f5a8f8fa9b6ee083552975a0eab4b96d22c7
gbbelskij <bleb_1@mail.ru>
2024-12-10 12:20:08 +0300

    done

---
+++ blogicum/pages/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def about(request):
+    print('about')
+    template = 'pages/about.html'
+    return render(request, template)
+
+def rules(request):
+    print('rules')
+    template = 'pages/rules.html'
+    return render(request, template)
